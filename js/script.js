ebcd1b1923ea610561ec4ff79a84b4e4f1e13605
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-02-10 17:18:45 +0100

    search for series too

--- js/script.js
+++ js/script.js
@@ -3,21 +3,37 @@ var app = new Vue({
   data: {
     apiKey: "4ab0d1d0499ee909a6c7be564ccd102f",
     language: "it",
-    search: "",
-    movies: []
+    searchField: "",
+    movies: [],
+    tvSeries: []
   },
   methods: {
-    searchMovies() {
+    search() {
+      // film
       axios
       .get("https://api.themoviedb.org/3/search/movie", {
         params: {
           api_key: this.apiKey,
           language: this.language,
-          query: this.search
+          query: this.searchField
         }
       })
       .then((result) => {
         this.movies = result.data.results;
+      })
+      .catch((error) => console.log(error));
+
+      // serie
+      axios
+      .get("https://api.themoviedb.org/3/search/tv", {
+        params: {
+          api_key: this.apiKey,
+          language: this.language,
+          query: this.searchField
+        }
+      })
+      .then((result) => {
+        this.tvSeries = result.data.results;
       })
       .catch((error) => console.log(error));
     }
