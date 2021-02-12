449b50466a43b156cb529bf6d9ce03f88a1ddea6
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-02-12 14:02:24 +0100

    cast

--- js/script.js
+++ js/script.js
@@ -11,42 +11,68 @@ var app = new Vue({
     // ricerca film
     searchMovies() {
       axios
-      .get("https://api.themoviedb.org/3/search/movie", {
-        params: {
-          api_key: this.apiKey,
-          language: this.language,
-          query: this.searchField
-        }
-      })
-      .then((result) => {
-        this.results = this.results.concat(result.data.results);
-        this.results.sort((a, b) => b.popularity - a.popularity);
-      })
-      .catch((error) => console.log(error));
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            api_key: this.apiKey,
+            language: this.language,
+            query: this.searchField
+          }
+        })
+        .then((result) => {
+
+          result.data.results.forEach((movie) => {
+            this.getCast(movie, "movie")
+          });
+
+          this.results = this.results.concat(result.data.results);
+          this.results.sort((a, b) => b.popularity - a.popularity);
+
+        })
+        .catch((error) => console.log(error));
     },
     // ricerca serie
     searchSeries() {
+
       axios
-      .get("https://api.themoviedb.org/3/search/tv", {
-        params: {
-          api_key: this.apiKey,
-          language: this.language,
-          query: this.searchField
-        }
-      })
-      .then((result) => {
-        this.results = this.results.concat(result.data.results);
-        this.results.sort((a, b) => b.popularity - a.popularity);
-      })
-      .catch((error) => console.log(error));
+        .get("https://api.themoviedb.org/3/search/tv", {
+          params: {
+            api_key: this.apiKey,
+            language: this.language,
+            query: this.searchField
+          }
+        })
+        .then((result) => {
+
+          result.data.results.forEach((series) => {
+            this.getCast(series, "tv")
+          });
+
+          this.results = this.results.concat(result.data.results);
+          this.results.sort((a, b) => b.popularity - a.popularity);
+        })
+        .catch((error) => console.log(error));
     },
     // ricerca film e serie
     search() {
+      this.c = 1;
       this.results = [];
       this.searchMovies();
       this.searchSeries();
     },
-    // contorllo per ridurre la lunghezza del campo overview
+    // aggiungere il cast
+    getCast(element, type) {
+
+      axios
+        .get(`https://api.themoviedb.org/3/movie/${element.id}/credits?api_key=${this.apiKey}`)
+        .then((result) => {
+          const newCast = result.data.cast.slice(0, 5);
+          Vue.set(element, "cast", newCast);
+          this.$forceUpdate();
+        })
+        .catch((error) => {console.log(error)})
+
+    },
+    // controllo per ridurre la lunghezza del campo overview
     lengthControl(text, max) {
       let newText = text.substring(0, max);
 
